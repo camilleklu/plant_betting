745710cbc82e85707c52194ea6088d8b659c19d5
Mahieux Celestine <etampes2@gmail.com>
2025-12-18 17:46:14 +0100

    ajout du css sur toutes les pages

--- bets/forms.py
+++ bets/forms.py
@@ -8,17 +8,22 @@ class BetForm(forms.ModelForm):
         fields = ['predicted_death_date', 'bet_amount']
         widgets = {
             'predicted_death_date': forms.DateTimeInput(
-                attrs={'type': 'datetime-local', 'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg'}
+                attrs={
+                    'type': 'date',  # Même avec DateTimeInput, vous pouvez utiliser type="date"
+                    'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg',
+                    'format': '%Y-%m-%d'  # Format d'affichage
+                }
             ),
             'bet_amount': forms.NumberInput(
                 attrs={'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg', 'placeholder': 'Votre mise'}
             ),
         }
-
+    
     def __init__(self, *args, **kwargs):
-        # On récupère les points de l'utilisateur passés par la vue
         user_points = kwargs.pop('user_points', 0)
         super().__init__(*args, **kwargs)
-        # On limite la mise au max de points de l'utilisateur (plafond à 500)
         max_bet = min(500, user_points)
         self.fields['bet_amount'].widget.attrs.update({'min': 10, 'max': max_bet})
+        
+        # Forcer le format de date pour l'affichage
+        self.fields['predicted_death_date'].input_formats = ['%Y-%m-%d']
